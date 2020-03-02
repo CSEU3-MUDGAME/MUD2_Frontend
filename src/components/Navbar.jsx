69c34acd06b63389a97fe9251c1fe85f651f624a
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-02 17:09:20 +0000

    Add route to navigate to game

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link, Route} from "react-router-dom";
 import Landing from './Landing';
 import Login from './Login';
 import Register from './Register';
+import MudMap from './Map';
 
 export default function Navbar(props){
     return(
@@ -16,6 +17,7 @@ export default function Navbar(props){
                 <Route exact path="/" component={Landing} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
+                <Route exact path="/play" component={MudMap} />
             </main>
         </div>
     );

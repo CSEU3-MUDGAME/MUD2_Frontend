3438b16eedec4102a75ab5ce83c3732fc50923dd
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-02 22:37:38 +0000

    Refactor axiosWithAuth fn to include appropriate Auth header

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export default function axiosWithAuth() {
     return axios.create({
       headers: {
         "Content-Type": "application/json",
-        Authorization: token
-      }
+        Authorization: `Token ${token}`,
+      },
     });
   }

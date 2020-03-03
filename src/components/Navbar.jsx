5dd19c4f7efe18f6db52f743f2d8ec458e9273fa
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-03 09:16:55 +0000

    Add basic navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,16 @@ import Login from './Login';
 import Register from './Register';
 import Game from './Game';
 import { ProtectedRoute } from '../utils/ProtectedRoute';
+import { StyledNavbar } from '../styles/navbar';
 
 export default function Navbar(props){
     return(
         <div>
-            <nav>
+            <StyledNavbar>
                 <Link to="/">Home</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
-            </nav>
+            </StyledNavbar>
             <main>
                 <Route exact path="/" component={Landing} />
                 <Route exact path="/login" component={Login} />
---
+++ src/styles/navbar.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledNavbar = styled.nav`
+    height: 10vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    a {
+        text-decoration: none;
+        color: black;
+    }
+`

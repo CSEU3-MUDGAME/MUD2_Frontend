2e27fd0099203b057a6b1239b0e713b6101735ef
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-03 08:23:29 +0000

    Display game details on initialization

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,24 +1,59 @@
-import React , { useEffect, useState } from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import React, { useEffect, useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-export default function Game(props){
-    const [ room, setRoom ] = useState();
+export default function Game(props) {
+  const [roomData, setRoomData] = useState();
 
-    useEffect(() => {
-        axiosWithAuth()
-        .get('https://lambda-mud-test.herokuapp.com/api/adv/init/')
-        .then(res => {
-            console.log(res)
-            setRoom(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }, []);
+  useEffect(() => {
+    axiosWithAuth()
+      .get("https://lambda-mud-test.herokuapp.com/api/adv/init/")
+      .then(res => {
+        console.log(res);
+        setRoomData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
+  const movePlayer = e => {
+    axiosWithAuth()
+      .post("https://lambda-mud-test.herokuapp.com/api/adv/move/", {
+        direction: e.target.id
+      })
+      .then(res => {
+        console.log(res);
+        setRoomData(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      Game
+      {!roomData ? (
+        "Loading"
+      ) : (
         <div>
-            Game
+          <p>Player: {roomData.name}</p>
+          <p>Location: {roomData.title}</p>
+          <p>Description: {roomData.description}</p>
+          <div>
+              <button id="n" onClick={movePlayer}>N</button>
+              <button id="s" onClick={movePlayer}>S</button>
+              <button id="e" onClick={movePlayer}>E</button>
+              <button id="w" onClick={movePlayer}>W</button>
+          </div>
+          <div style={{"maxHeight": "200px", "overflow": "hidden"}}>
+            Other Players:
+            {roomData.players.map(player => (
+              <p key={player}>{player}</p>
+            ))}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }

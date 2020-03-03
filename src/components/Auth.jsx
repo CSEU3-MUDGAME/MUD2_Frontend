8b4e7bfdb488c7e4c7d0139dabfcf1ae0138f135
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-03 09:31:22 +0000

    Collapse auth forms to one

---
+++ src/components/Auth.jsx
@@ -0,0 +1,109 @@
+import React, { useRef, useState } from "react";
+import axios from "axios";
+
+export default function Auth(props) {
+  const [authType, setAuthType] = useState("login");
+  return (
+    <>
+      {authType === "login" ? <Login /> : <Register />}
+      {authType === "login" ? (
+        <p>
+          Don't have an account yet? Register{" "}
+          <span onClick={() => setAuthType("register")}>here</span>
+        </p>
+      ) : (
+        <p>
+          Already have an account? Login{" "}
+          <span onClick={() => setAuthType("login")}>here</span>
+        </p>
+      )}
+    </>
+  );
+}
+
+function Login(props) {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handlelogin = () => {
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/login/", {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value
+      })
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.key);
+        props.history.push("/play");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <h1>Log In</h1>
+      <input
+        name="username"
+        type="text"
+        ref={usernameRef}
+        placeholder="Username"
+      />
+      <input
+        name="password"
+        type="password"
+        ref={passwordRef}
+        placeholder="Password"
+      />
+      <button onClick={handlelogin}>Go</button>
+    </div>
+  );
+}
+
+function Register(props) {
+  const usernameRef = useRef();
+  const password1Ref = useRef();
+  const password2Ref = useRef();
+
+  const handleregister = e => {
+    e.preventDefault();
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/registration/", {
+        username: usernameRef.current.value,
+        password1: password1Ref.current.value,
+        password2: password2Ref.current.value
+      })
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.key);
+        props.history.push("/play");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <h1>Register</h1>
+      <input
+        name="username"
+        type="text"
+        ref={usernameRef}
+        placeholder="Username"
+      />
+      <input
+        name="password1"
+        type="password"
+        ref={password1Ref}
+        placeholder="Password"
+      />
+      <input
+        name="password2"
+        type="password"
+        ref={password2Ref}
+        placeholder="Confirm Password"
+      />
+      <button onClick={handleregister}>Go</button>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link, Route} from "react-router-dom";
 import Landing from './Landing';
-import Login from './Login';
-import Register from './Register';
+import Auth from './Auth';
 import Game from './Game';
 import { ProtectedRoute } from '../utils/ProtectedRoute';
 import { StyledNavbar } from '../styles/navbar';
@@ -13,12 +12,10 @@ export default function Navbar(props){
             <StyledNavbar>
                 <Link to="/">Home</Link>
                 <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
             </StyledNavbar>
             <main>
                 <Route exact path="/" component={Landing} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Auth} />
                 <ProtectedRoute path="/play" component={Game} />
             </main>
         </div>

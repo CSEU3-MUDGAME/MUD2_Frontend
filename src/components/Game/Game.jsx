130c9c1bb61d9b63becb300ccb4b89a3789d37d6
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-04 16:48:04 +0000

    Remove redundant functions

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -8,30 +8,7 @@ export default function Game(props) {
   const [gameData, setGameData] = useState();
 
   useEffect(() => {
-    axiosWithAuth()
-      .get("https://lambda-mud-test.herokuapp.com/api/adv/init/")
-      .then(res => {
-        console.log(res);
-        setGameData(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
   }, []);
-
-  const movePlayer = e => {
-    axiosWithAuth()
-      .post("https://lambda-mud-test.herokuapp.com/api/adv/move/", {
-        direction: e.target.id
-      })
-      .then(res => {
-        console.log(res);
-        setGameData(res.data)
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
 
   return (
     <StyledGameWrapper>

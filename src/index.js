8d10eb3635e59ca3a21b86af426178c7caba52c5
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-04 16:47:43 +0000

    Add store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import * as reducers from './store/reducers';
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+    player: reducers.playerReducer
+})
+
+const store = createStore(
+    rootReducer,
+    {},
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

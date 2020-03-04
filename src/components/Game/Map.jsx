85e9cc2418f46d190f34fc02fb39e103d9070b5b
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-04 16:12:08 +0000

    Add Player component

--- src/components/Game/Map.jsx
+++ src/components/Game/Map.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { StyledMap } from '../../styles/map';
 import { mockMap } from './mockMap';
+import Player from './Player';
+
+
+const playerData = {
+  currentRoom: 0
+}
 
 export default function Map(props) {
+ const {gameData, setGameData, movePlayer} = props;
     return (
       <StyledMap>
           {mockMap.map(row => 
             <div className="column">
               {row.map(room => {
-              return <div className={`room n-wall-${room.sides.up} s-wall-${room.sides.down} e-wall-${room.sides.right} w-wall-${room.sides.left} wall-l-${room.id < 30} wall-r-${room.id >= 870} wall-t-${room.id % 30 === 0} `}>
+              return <div className={`room n-wall-${room.sides.up} s-wall-${room.sides.down} e-wall-${room.sides.right} w-wall-${room.sides.left} wall-l-${room.id < 30} wall-r-${room.id >= 870} wall-t-${room.id % 30 === 0} `}> 
+                <>{room.id === playerData.currentRoom ? <Player gameData={gameData} setGameData={setGameData} movePlayer={movePlayer}/> : null}</>
                </div>
               })}
             </div>
---
+++ src/components/Game/Player.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function Player(props) {
+    const {gameData, setGameData, movePlayer} = props;
+
+    return (
+      <div>
+          here
+      </div>
+    );
+  }

39feb3dacbb3a6695444c60cd6421e6633454618
David Kuseh <davegru203@gmail.com>
2020-03-05 22:21:26 +0000

    styling tweaks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
-import Landing from "./Landing";
 import Auth from "./Auth";
 import About from "./About";
 import Game from "./Game/Game";
@@ -12,12 +11,10 @@ export default function Navbar(props) {
     <div>
       <StyledNavbar>
         <Link to="/">Robomaze</Link>
-        <Link to="/login">Login</Link>
         <Link to="/about">About</Link>
       </StyledNavbar>
       <main>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/login" component={Auth} />
+        <Route exact path="/" component={Auth} />
         <Route exact path="/about" component={About} />
         <ProtectedRoute path="/play" component={Game} />
       </main>
--- src/styles/auth.js
+++ src/styles/auth.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const AuthStyle = styled.div`
         display: flex;
         flex-direction: column;
-        margin: 0 auto;
+        margin: 80px auto;
 
         input {
             margin: 20px auto;
@@ -37,5 +37,9 @@ export const AuthStyle = styled.div`
         span {
             text-decoration:underline;
             color:#e25822
+        }
+
+        p {
+            margin-top: -40px;
         }
 `

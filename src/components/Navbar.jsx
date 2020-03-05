cde146784fa62b84fc52e515cc163a4c115b8b6c
Megan Ennis <Megan@Megans-iMac-9.local>
2020-03-05 19:32:41 +0000

    Add auth flow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import { Link, Route} from "react-router-dom";
-import Landing from './Landing';
-import Auth from './Auth';
-import Game from './Game/Game';
-import { ProtectedRoute } from '../utils/ProtectedRoute';
-import { StyledNavbar } from '../styles/navbar';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Landing from "./Landing";
+import Auth from "./Auth";
+import Game from "./Game/Game";
+import { ProtectedRoute } from "../utils/ProtectedRoute";
+import { StyledNavbar } from "../styles/navbar";
 
-export default function Navbar(props){
-    return(
-        <div>
-            <StyledNavbar>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-            </StyledNavbar>
-            <main>
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/login" component={Auth} />
-                <ProtectedRoute path="/play" component={Game} />
-            </main>
-        </div>
-    );
+export default function Navbar(props) {
+  const token = localStorage.getItem("token");
+  return (
+    <div>
+      <StyledNavbar>
+        {!token ? (
+          <Link to="/login">Login</Link>
+        ) : (
+          <button
+            onClick={() => {
+              props.history.push("/");
+              localStorage.removeItem("token");
+            }}
+          >
+            Logout
+          </button>
+        )}
+      </StyledNavbar>
+      <main>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Auth} />
+        <ProtectedRoute path="/play" component={Game} />
+      </main>
+    </div>
+  );
 }
